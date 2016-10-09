f0d8b4d45b011287e56523792f544db5aa9e3eec
koshigoe <koshigoeb@gmail.com>
2016-10-09 21:00:43 +0900

    $ bundle gem aws-ssm-console

---
+++ lib/aws/ssm/console.rb
@@ -0,0 +1,9 @@
+require "aws/ssm/console/version"
+
+module Aws
+  module Ssm
+    module Console
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/aws/ssm/console/version.rb
@@ -0,0 +1,7 @@
+module Aws
+  module Ssm
+    module Console
+      VERSION = "0.1.0"
+    end
+  end
+end
