98ce36033b3cf7c1320469f1c1e5171c27ce39b0
koshigoe <koshigoeb@gmail.com>
2016-12-21 20:12:34 +0900

    Remove unnecessary file.

--- lib/aws/ssm/console/version.rb
+++
@@ -1,9 +0,0 @@
-module Aws
-  module SSM
-    module Console
-      GEMSPEC_PATH = File.expand_path('../../../../../aws-ssm-console.gemspec', __FILE__)
-      GEMSPEC = Gem::Specification.load(GEMSPEC_PATH)
-      VERSION = GEMSPEC.version.to_s.freeze
-    end
-  end
-end
